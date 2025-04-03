330e7d73bd651e6653d7c6c3f3d02aa481d9c807
santiagoartgut9 <69946591+santiagoartgut9@users.noreply.github.com>
2025-04-03 00:11:36 -0500

    Mensaje de commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from "./components/NavBar";
+import ConsultaDisponibilidad from './components/ConsultaDisponibilidad';
+import ReservarLaboratorio from './components/ReservarLaboratorio';
+import CancelarReserva from './components/CancelarReserva';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-4"><ConsultaDisponibilidad /></div>
+          <div className="col-md-4"><ReservarLaboratorio /></div>
+          <div className="col-md-4"><CancelarReserva /></div>
+        </div>
+      </div>
     </div>
   );
 }
